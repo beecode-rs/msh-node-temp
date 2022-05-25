8182ec24250dc74574141b851d16b842524ceb9f
Milos Bugarinovic <milos.bugarinovic@gmail.com>
2022-05-25 09:09:39 +0200

    chore: update package.json and small fix

--- src/app/cli-app.ts
+++ src/app/cli-app.ts
@@ -2,7 +2,7 @@ import { App } from '@beecode/msh-node-app'
 import { ExecArgsAsCommand } from 'src/app/init/exec-args-as-command'
 
 export class CliApp extends App {
-  constructor(args: string[]) {
+  public constructor(args: string[]) {
     super(new ExecArgsAsCommand(args))
   }
 }
